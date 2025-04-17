806999523ced0e677943d0241b7d62f1456be613
FreeGameE <a.kolesnicov@mail.ru>
2025-04-17 18:22:43 +0300

    start

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -4,7 +4,7 @@ import { tokenManager } from "../services/tokenManager";
 
 const api = axios.create({
   baseURL: "https://easydev.club/api/v1",
-  timeout: 10000,
+  timeout: 5000,
   headers: {
     "Content-Type": "application/json; charset=UTF-8",
   },
