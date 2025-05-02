228547298b98a0f05a4007db880cd8533667d37b
FreeGameE <a.kolesnicov@mail.ru>
2025-05-02 19:37:31 +0300

    апи приведены в порядок

--- src/components/mainApp/UsersPage/UsersPage.tsx
+++ src/components/mainApp/UsersPage/UsersPage.tsx
@@ -1,5 +1,20 @@
+import { Flex, Typography } from "antd";
+import "./UsersPage.css"
+
 const UsersPage: React.FC = () => {
-  return (<div>123</div>);
+  return (
+    <Flex vertical align="center">
+      <Typography.Title
+        level={2}
+        style={{ color: "white", textAlign: "center", marginTop: "0.6rem" }}
+      >
+        ПОЛЬЗОВАТЕЛИ
+      </Typography.Title>
+      <Flex vertical className="users-board">
+123
+      </Flex>
+    </Flex>
+  );
 };
 
 export default UsersPage;
